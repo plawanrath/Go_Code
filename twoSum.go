3c5c4b25b1d14781889f02ad4212afbf4eaad82e
Plawan Rath <plawanrath@gmail.com>
2018-03-22 21:15:21 -0400

    two sum solution

--- twoSum.go
+++ twoSum.go
@@ -3,5 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Test")
+	nums := []int{2, 7, 11, 15}
+	target := 9
+	result := twoSum(nums, target)
+	fmt.Println(result)
+}
+
+func twoSum(nums []int, target int) []int {
+	res := make([]int, 0)
+	m := make(map[int]int)
+	for i := 0; i < len(nums); i++ {
+		complement := target - nums[i]
+		if val, ok := m[complement]; ok {
+			res = append(res, val)
+			res = append(res, i)
+		} else {
+			m[nums[i]] = i
+		}
+	}
+	return res
 }
